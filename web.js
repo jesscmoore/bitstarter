abe6f2c136a23ee7af9fde0927df6eccd79b3cf2
Ubuntu <ubuntu@ip-172-31-43-212.us-west-2.compute.internal>
2013-09-02 15:00:58 +0000

    Finally fixed web.js

--- web.js
+++ web.js
@@ -7,7 +7,6 @@ var buffer = fs.readFileSync("index.html");
 var out_string = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
   response.send(out_string);
 });
 
