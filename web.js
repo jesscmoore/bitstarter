59853e920cef7b45ff7d066d9dc7e189736867b4
Ubuntu <ubuntu@ip-172-31-43-212.us-west-2.compute.internal>
2013-09-02 14:44:32 +0000

    Edited web.js to load string from buffer

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var app = express();
 app.use(express.logger());
 
 var buffer = fs.readFileSync("index.html");
-
+var out_string = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-  response.send(buffer.toString('utf-8'));
+  response.send(out_string);
 });
 
 var port = process.env.PORT || 5000;
